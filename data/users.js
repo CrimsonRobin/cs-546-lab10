b8007f0a5c09f8cff58e8f8b5abe3152d0ea2608
CrimsonRobin <CrimsonRobin807@outlook.com>
2024-04-16 22:15:10 -0400

    Updated Mongo Config and Finished Data functions

--- data/users.js
+++ data/users.js
@@ -1,4 +1,8 @@
 //import mongo collections, bcrypt and implement the following data functions
+import helper from "../helpers.js";
+import bcrypt from "bcrypt";
+import {users} from "../config/mongoCollections.js"
+
 export const registerUser = async (
   firstName,
   lastName,
@@ -7,6 +11,61 @@ export const registerUser = async (
   favoriteQuote,
   themePreference,
   role
-) => {};
+) => {
+  firstName = helper.checkString("First Name", firstName, true, 2, 25);
+  lastName = helper.checkString("Last Name", lastName, true, 2, 25);
+  username = helper.checkString("Username", username.toLowerCase(), true, 5, 10);
+  password = helper.checkString("Password", password, false, 8);
+  helper.checkPassword(password);
+  favoriteQuote = helper.checkString("Favorite Quote", favoriteQuote, true, 20, 255);
+  themePreference = helper.validValues("Theme Preference", themePreference.toLowerCase(), "light", "dark");
+  role = helper.validValues("Role", role.toLowerCase(), "admin", "user");
 
-export const loginUser = async (username, password) => {};
+  const hash = await bcrypt.hash(password, 16);
+
+  let NewUser = {
+    firstName: firstName,
+    lastName: lastName,
+    username: username,
+    password: hash,
+    favoriteQuote: favoriteQuote,
+    themePreference: themePreference,
+    role: role
+  };
+
+  const userCollection = await users();
+
+  if(userCollection.findOne({username: username}) !== null) {
+    throw new Error(`There is already a user with that username.`);
+  } //check if username is in the database
+
+  const info = userCollection.insertOne(NewUser);
+  if (!info.acknowledged || !info.insertedId) {
+    throw new Error("Could not add user.");
+  }
+
+  return {signupCompleted: true};
+
+};
+
+export const loginUser = async (username, password) => {
+  username = helper.checkString("Username", username.toLowerCase(), true, 5, 10);
+  password = helper.checkString("Password", password, false, 8);
+  helper.checkPassword(password);
+
+  const userCollection = await users();
+
+  const user = userCollection.findOne({username: username});
+  if(user === null) {
+    throw new Error(`Either the username or password is invalid`);
+  } //check if username is in the database
+
+  const match = await bcrypt.compare(password, user.password);
+
+    if(!match) {
+        throw new Error(`Either the username or password is invalid`);
+    }
+
+    return {firstName: user.firstName, lastName: user.lastName, favoriteQuote: user.favoriteQuote, 
+      themePreference: user.themePreference, role: user.role};
+};
--- helpers.js
+++ helpers.js
@@ -1,1 +1,60 @@
 //You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
+const exportedMethods = {
+    checkString(name, str, noNum, min, max) {
+        if(!str) {
+            throw new Error(`${name} is undefined.`);
+        }
+        if(typeof str !== "string") {
+            throw new Error(`${name} is not a string.`);
+        }
+        str = str.trim();
+        if(str.length === 0) {
+            throw new Error(`${name} is empty.`);
+        }
+
+        if(noNum) {
+            if(str.match(/\d/) !== null) {
+                throw new Error(`${name} contains numbers.`);
+            }
+        }
+
+        if(min) {
+            if(str.length <= min) {
+                throw new Error(`${name} is less than ${min} characters long.`);
+            }
+        }
+        if(max) {
+            if(str.length >= max) {
+                throw new Error(`${name} is greater than ${max} characters long.`);
+            }
+        }
+
+        return str;
+    },
+    checkPassword(password) {
+        //will be called after checkString
+        if(!password) {
+            throw new Error(`Password is not provided.`);
+        }
+
+        if(password.match(/[A-Z]/) === null) {
+            throw new Error(`Password needs at least one uppercase character.`);
+        }
+        if(password.match(/\d/) === null) {
+            throw new Error(`Password needs at least one number.`);
+        }
+        if(password.match(/[#?!@$ %^&*-]/) === null) {
+            throw new Error(`Password needs at least one special character.`);
+        }
+    },
+    validValues(name, str, v1, v2) {
+        //checks if given string is one of the two values
+        if(str !== v1 && str !== v2) {
+            throw new Error(`Did not supply a valid value for ${name}`);
+        }
+
+        return str;
+    }
+};
+
+export default exportedMethods;
